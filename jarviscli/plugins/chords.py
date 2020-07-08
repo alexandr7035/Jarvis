4f3c791cf799153df805551204d9197cd6e76a85
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-07-08 23:38:48 +0300

    Create chords json and load data

--- jarviscli/plugins/chords.py
+++ jarviscli/plugins/chords.py
@@ -1,8 +1,10 @@
 import os
+import json
 from plugin import plugin, alias
 from colorama import Fore
 
 PLUGIN_PATH = os.path.abspath(os.path.dirname(__file__))
+CHORDS_FILE = os.path.join(PLUGIN_PATH, "../data/chords.json")
 
 # List for guitar neck
 # 6 strings, 12 flats
@@ -30,3 +32,13 @@ class ChordsPlugin():
 
     def __call__(self, jarvis, s):
         jarvis.say(str(EMPTY_NECK))
+
+        self.get_chord("C")
+
+    def get_chord(self, chord_name):
+        with open(CHORDS_FILE) as f:
+            chords = json.load(f)
+
+        chord_data = chords[chord_name]
+
+        print(chord_data)
